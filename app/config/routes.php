bcc353d6597f0d24d367b8fb17693a2595c4d642
wrysha <201246353+wrysha@users.noreply.github.com>
2026-09-07 15:22:47 +0800

    Open Student Home at the site root

--- app/config/routes.php
+++ app/config/routes.php
@@ -44,7 +44,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 */
 /** @var object $router **/
 
-/**$router->get('/', 'Welcome::index');**/
+$router->get('/', 'StudentController::index');
 $router->get('/student', 'StudentController::index');
 $router->get('/student/profile', 'StudentController::profile')
        ->middleware('student');
